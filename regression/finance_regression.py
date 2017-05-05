1265cad9fa55971ee93c0169148906b79eaf782f
Niraj Warade <nraj.c.war@gmail.com>
2017-05-05 18:40:23 +0530

    Add files via upload

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -14,6 +14,7 @@
 
 
 import sys
+from time import time
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -23,13 +24,13 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
-target, features = targetFeatureSplit( data )
+target, features = targetFeatureSplit(data)
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +39,24 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+
+t0 = time()
+reg.fit(feature_train,target_train)
+print "training time:", round(time()-t0, 3), "s"
 
 
+t1 = time()
+pred=reg.predict(feature_test)
+print "testing time:", round(time()-t1, 3), "s"
 
 
+print "Slope:",reg.coef_
+print "Intercept:",reg.intercept_
 
+print "R-Squared Score:",reg.score(feature_test,target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -56,15 +70,17 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
